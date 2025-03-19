9f64a751d08ff8ec8e7ffec43e8347a6a2994a45
Radamir <dermeister20032132134@gmail.com>
2025-03-19 10:52:55 +0300

    добавил базовые методы из условия

--- Library.cpp
+++ Library.cpp
@@ -3,3 +3,22 @@
 //
 
 #include "Library.h"
+#include <algorithm>
+std::shared_ptr<Book> Library::parseBook(const Book &book) const {
+  return std::make_shared<Book>(book);
+}
+Library::bookIteratorType Library::getBookIterator(const Book& book) const{
+  return
+}
+
+bool Library::findBook(const Book& book) const{
+  return getBookIterator(book) != books.end();
+}
+void Library::addBook(const Book& book){
+  books.push_back(parseBook(book));
+}
+void Library::removeBook(const Book& book){
+  if (findBook(book)){
+     books.erase(parseBook(book));
+  }
+}
--- Library.h
+++ Library.h
@@ -5,10 +5,19 @@
 #ifndef LIBRARY_H
 #define LIBRARY_H
 
-
-
+#include <vector>
+#include<memory>
+#include "Book.h"
 class Library {
-
+    typedef  std::vector<std::shared_ptr<Book>>::const_iterator bookIteratorType;
+    std::vector<std::shared_ptr<Book>> books;
+    std::shared_ptr<Book> parseBook(const Book& book) const;
+    bookIteratorType getBookIterator(const Book& book) const;
+    public:
+    Library() = default;
+    bool findBook(const Book& book) const;
+    void addBook(const Book& book);
+    void removeBook(const Book& book);
 };
 
 
